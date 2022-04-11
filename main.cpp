b3e2f1c5b0ef6f339110c8d6c7a6e742523cc1b4
nemmi1001 <91687500+nemmi1001@users.noreply.github.com>
2022-04-11 15:48:53 +0200

    initial cummit

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+void wall(double x_0, double y_0, double x_end, double y_end, double dp, double ort){ ///ort orientace normály
+//	if(y_end == y_0){double n = 1+ (x_end - x_0)/dp;}
+//	else if(x_end == x_0){double n = 1+ (y_end - y_0)/dp;}
+//	else {double dp }	
+	double l=sqrt((pow(x_end-x_0,2)+ pow(y_end - y_0,2) ));
+	double  nn =1 + l/dp;
+	if() /// rozhodnutí jestli je nn přirozené číslo
+	else	
+	for(int i = 0){
+		
+		}
+
+			}
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		return 0;}
