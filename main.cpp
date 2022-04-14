f3c8f894698011d7014688bb17a5bd7d2d29778f
marhatom <tomasmarhan@gmail.com>
2022-04-14 23:43:56 +0200

    struktura particle

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@
 
 using namespace std;
 
-void wall(double x_0, double y_0, double x_end, double y_end, double dp); // double ort - ort orientace normály
-void save(vector<double>& x, vector<double>& y);
+typedef struct{
+	double x, y, nx, ny;
+} particle;
 
+void wall(double x_0, double y_0, double x_end, double y_end, double dp); // double ort - ort orientace normály
+void save(vector<particle>& P);
 
 
 int main(){
@@ -20,36 +23,40 @@ int main(){
 
 
 void wall(double x_0, double y_0, double x_end, double y_end, double dp){ //double ort
-	vector<double> x, y;	
-
+	vector<double> x, y, nx, ny;	
+	
 	double l  = sqrt((pow(x_end-x_0,2) + pow(y_end-y_0,2)));
-	double nn = l/dp;
+	double nn = l/dp + 1;
+	vector<particle> P(nn);
 	double smer[] = {(x_end-x_0) / l, (y_end-y_0) / l};
 
-	x.push_back(x_0);
-	y.push_back(y_0);
+	P[0].x  = x_0;
+	P[0].y  = y_0;
+	P[0].nx = -smer[1];
+	P[0].ny =  smer[0];
 
 	if (round(nn) != nn) {
 		printf("\n[Error] Pocet castic neni cele cislo: %lf\n", nn);
 		exit(1);
 	}
 	else	
-		for(int i = 0; i < nn; i++) {
-			x.push_back(x[i] + dp * smer[0]);
-			y.push_back(y[i] + dp * smer[1]);
+		for(int i = 1; i < nn; i++) {
+			P[i].x  = P[i-1].x + dp * smer[0];
+			P[i].y  = P[i-1].y + dp * smer[1];
+			P[i].nx = -smer[1];
+			P[i].ny =  smer[0];
 		}
-
-	save(x, y);
+	
+	save(P);
 }
 
-void save(vector<double>& x, vector<double>& y) {
+void save(vector<particle>& P) {
 	ofstream vystup("wall.txt");
 
-	for(int i = 0; i < x.size(); i++) {
+	for(int i = 0; i < P.size(); i++) {
 		vystup << fixed << setprecision(2);
-		vystup << x[i] << "\t" << y[i] << endl;
+		vystup << P[i].x << "\t" << P[i].y << "\t" << P[i].nx << "\t" << P[i].ny << endl;
 	}
 
 	vystup.close();
-
 }
